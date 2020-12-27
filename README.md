7275c55243792a135bfbab38d00e9d50511585a6
Maxim Van de Wynckel <maxim@mvdw-software.com>
2020-12-27 20:17:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OpenHPS CLI Tool
+## See https://dev.to/duwainevandriel/build-your-own-project-template-generator-59k4
---
+++ src/templates/openhps-module/test/specs/example.spec.ts
@@ -0,0 +1,4 @@
+import 'mocha';
+import {
+    Model, ModelBuilder, DataObject, Absolute2DPosition, DataFrame, CallbackSinkNode, TimedPullNode, TimeUnit
+} from '@openhps/core';
